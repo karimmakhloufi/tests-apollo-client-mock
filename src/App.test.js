3b58bd58109829937119ea3d6ac8407f7b0e6549
Karim Makhloufi <karim.makhloufi.paris@gmail.com>
2021-11-23 11:56:08 +0100

    Added loading test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,14 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import { MockedProvider } from "@apollo/client/testing";
+import App from "./App";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+const mocks = [];
+
+test("renders learn react link", () => {
+  render(
+    <MockedProvider mocks={mocks} addTypename={false}>
+      <App />
+    </MockedProvider>
+  );
+  expect(screen.queryByText(/Loading/i)).toBeInTheDocument();
 });
